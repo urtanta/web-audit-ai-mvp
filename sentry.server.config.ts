404bd44186ab449ec110006f470b528d85e38270
Urtanta <35062122+urtanta@users.noreply.github.com>
2025-04-20 22:00:49 +0200

    Update sentry.server.config.ts

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,6 +1,6 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: https://6c3aa3ccebaa7c591404412296a15c80@o4509187350593536.ingest.de.sentry.io/4509187353083984,
+  dsn: "https://6c3aa3ccebaa7c591404412296a15c80@o4509187350593536.ingest.de.sentry.io/4509187353083984",
   tracesSampleRate: 1.0,
 });
